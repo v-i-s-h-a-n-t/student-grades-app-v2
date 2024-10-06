ec9308032a131ae52a809de8be9f76a2dd026f4c
Vishant Kumar <163496021+v-i-s-h-a-n-t@users.noreply.github.com>
2024-10-06 18:04:22 +1200

    feat: add student class definition feat: add student class implementation

--- student-grades-app-v2/student.cpp
+++ student-grades-app-v2/student.cpp
@@ -4,8 +4,38 @@
 
 using  namespace std;
 
-Student::Student() {
+Student::Student() 
+{
 	cout << "Student" << endl;
 }
+Student::~Student()
+{
+}
 
+void Student::setId(string id)
+{
+	this->id = id;
+}
+string Student::getId()
+{
+	return id;
+}
+
+void Student::setCoursework(double coursework)
+{
+	this->coursework = coursework;
+}
+double Student::getCoursework()
+{
+	return coursework;
+}
+
+void Student::setFinalExam(double coursework)
+{
+	this->finalExam = finalExam;
+}
+double Student::getFinalExam()
+{
+	return finalExam;
+}
 
--- student-grades-app-v2/student.h
+++ student-grades-app-v2/student.h
@@ -4,7 +4,22 @@
 
 class Student
 {
+private:
+	string id;
+	double coursework;
+	double finalExam;
+
 public:
 	Student();
+	~Student();
+
+	void setId(string id);
+	string getId();
+
+	void setCoursework(double coursework);
+	double getCoursework();
+
+	void setFinalExam(double finalExam);
+	double getFinalExam();
 };
 
